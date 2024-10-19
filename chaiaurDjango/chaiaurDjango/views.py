3cdc69c05adb96ea0626fd939f04419cb1f4f91f
Saikat123Dev <berasaikat729@gmail.com>
2024-10-19 16:37:11 +0530

    django

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
    # return HttpResponse("Hello world you are at chai aur django home page")
-   return render(request,'index.html')
+   return render(request,'website/index.html')
 
 def about(request):
     return HttpResponse("hello world you are at about page")
